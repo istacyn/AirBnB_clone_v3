2ef018899020dcd7e35f2d8c79a8b6a9f85e5730
Ivy <ivystacyn@gmail.com>
2023-07-31 11:51:06 +0000

    pycodestyle corrections task 13

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -8,7 +8,8 @@ from models.amenity import Amenity
 from api.v1.views import app_views
 
 
-@app_views.route('/places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities', methods=['GET'],
+                 strict_slashes=False)
 def get_amenities_by_place(place_id):
     place = storage.get(Place, place_id)
     if place is None:
@@ -18,7 +19,8 @@ def get_amenities_by_place(place_id):
     return jsonify(amenities)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity_from_place(place_id, amenity_id):
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
@@ -31,7 +33,8 @@ def delete_amenity_from_place(place_id, amenity_id):
     return jsonify({}), 200
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'], strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
+                 strict_slashes=False)
 def link_amenity_to_place(place_id, amenity_id):
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
