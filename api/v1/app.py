64d81c2abe038776061130379b983674c16fd1eb
Ivy <ivystacyn@gmail.com>
2023-07-31 11:35:54 +0000

    Task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 """
 app.py file
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -11,6 +11,7 @@ from flask_cors import CORS
 
 """Create a Flask app"""
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 # Set up CORS
 CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
@@ -18,16 +19,17 @@ CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 """Register the blueprint app_views to the Flask instance app"""
 app.register_blueprint(app_views)
 
-"""Method to handle teardown after each request"""
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
+    """Method to handle teardown after each request"""
     storage.close()
 
 
-"""Custom error handler for 404 errors"""
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({"error": "Not found"}), 404
+    """Custom error handler for 404 errors"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
