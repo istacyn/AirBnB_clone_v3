3171749e8722786197951e5d430b6b10f72d7314
root <jenyoolumide@.com>
2023-07-31 02:15:09 +0000

    api updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,6 +20,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     storage.close()
 
+"""Custom error handler for 404 errors"""
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     """Get the host and port from environment variables or use default values"""
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
