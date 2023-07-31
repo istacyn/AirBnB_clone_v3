05739a5b151c4fad27bdf128dfd4bed5795e79c6
Ivy <ivystacyn@gmail.com>
2023-07-31 07:38:56 +0000

    Task 5 api/v1/app.py pycodestyle validation

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,6 +23,7 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     storage.close()
 
+
 """Custom error handler for 404 errors"""
 @app.errorhandler(404)
 def not_found(error):
@@ -30,7 +31,9 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    """Get the host and port from environment variables or use default values"""
+    """Get the host and port from environment variables
+    or use default values
+    """
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
     port = int(os.environ.get("HBNB_API_PORT", 5000))
     """Run the Flask app"""
