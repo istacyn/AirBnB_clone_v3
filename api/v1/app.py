21cec575be281cd22cea5668d590054e2a6730bb
root <jenyoolumide@.com>
2023-07-31 01:11:14 +0000

    api update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-api flask
+app.py file
 """
-from os import getenv
-from flask import Flask, jsonify, Blueprint
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
 
+"""Create a Flask app"""
 app = Flask(__name__)
-CORS(app, origins="0.0.0.0")
+
+"""Register the blueprint app_views to the Flask instance app"""
 app.register_blueprint(app_views)
-CORS(app_views)
 
-
+"""Method to handle teardown after each request"""
 @app.teardown_appcontext
-def close_db_sesion(error):
-    """ this for slash routing"""
+def teardown_appcontext(exception):
     storage.close()
 
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """handler for 404 errors that returns a JSON-formatted
-    404 status code response.
-    """
-    return ({'error': 'Not found'}), 404
-
-
 if __name__ == "__main__":
-    HBNB_API_HOST = getenv('HBNB_API_HOST')
-    HBNB_API_PORT = getenv('HBNB_API_PORT')
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT,
-            threaded=True, debug=True)
+    """Get the host and port from environment variables or use default values"""
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    """Run the Flask app"""
+    app.run(host=host, port=port, threaded=True)
